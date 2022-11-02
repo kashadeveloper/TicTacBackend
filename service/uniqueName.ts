cc976dc9fd6cbc9d8ab50491b70ea00e40bda196
kashadeveloper <79090088+kashadeveloper@users.noreply.github.com>
2022-11-02 20:35:32 +0300

    service

--- service/uniqueName.ts
+++ service/uniqueName.ts
@@ -1,7 +1,7 @@
 import { uniqueNamesGenerator, Config, adjectives, colors, animals } from 'unique-names-generator';
 
 const customConfig: Config = {
-  dictionaries: [adjectives, colors, animals],
+  dictionaries: [adjectives, colors],
   separator: '-',
   length: 2,
 };
